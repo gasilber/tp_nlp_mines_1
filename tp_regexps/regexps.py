b1204ece8c9adfb456976716aab9cd7ed34d5159
nathanael de La Chesnais <nathanael.houitte-de-la-chesnais@polytechnique.edu>
2024-12-13 11:41:05 +0100

    regex ecli

--- tp_regexps/regexps.py
+++ tp_regexps/regexps.py
@@ -1,4 +1,4 @@
 import re
 
 decision_ref_re = re.compile(r'<a\s+href="/decision/(?P<id>[^\?]+)')
-ecli_re = re.compile(r'TODO')
+ecli_re = re.compile(r'<p>(?P<ecli>ECLI:[A-Z]{2}:[A-Z]{5}:\d{4}:[A-Z0-9]{7})</p>')
