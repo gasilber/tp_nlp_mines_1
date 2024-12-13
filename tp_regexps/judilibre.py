e8617f8cb3647a442707c381c165ec178a8ecb99
Shaamallow <eyal.benaroche@gmail.com>
2024-12-13 13:29:37 +0100

    add black and isort dep
    - add formatting to repo
    - add github action for auto check

--- tp_regexps/judilibre.py
+++ tp_regexps/judilibre.py
@@ -5,74 +5,69 @@ from tp_regexps import regexps
 
 class Judilibre(object):
 
-	base_url = "https://www.courdecassation.fr"
+    base_url = "https://www.courdecassation.fr"
 
-	base_search_url = (
-		"{url}/recherche-judilibre?"
-		"search_api_fulltext={search_api_fulltext}"
-		"&date_du={date_du}"
-		"&date_au={date_au}"
-		"&judilibre_juridiction={juridiction}"
-		"&op={op}"
-		"&sort={sort}"
-		"&items_per_page={items_per_page}"
-		"&page={page}"
-	)
+    base_search_url = (
+        "{url}/recherche-judilibre?"
+        "search_api_fulltext={search_api_fulltext}"
+        "&date_du={date_du}"
+        "&date_au={date_au}"
+        "&judilibre_juridiction={juridiction}"
+        "&op={op}"
+        "&sort={sort}"
+        "&items_per_page={items_per_page}"
+        "&page={page}"
+    )
 
-	base_decision_url = (
-		"{url}/decision/{id}"
-	)
+    base_decision_url = "{url}/decision/{id}"
 
-	user_agent = (
-		"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7)"
-		" AppleWebKit/605.1.15 (KHTML, like Gecko)"
-		" Version/18.0.1 Safari/605.1.15"
-	)
+    user_agent = (
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7)"
+        " AppleWebKit/605.1.15 (KHTML, like Gecko)"
+        " Version/18.0.1 Safari/605.1.15"
+    )
 
-	def __init__(self):
-		self.juridiction = "cc"
-		self.op = "Rechercher%20sur%20judilibre"
-		self.date_du = ''
-		self.date_au = ''
-		self.search_api_fulltext = ''
-		self.sort = 'date-desc'
-		self.items_per_page = 30
-		self.session = HTMLSession()
+    def __init__(self):
+        self.juridiction = "cc"
+        self.op = "Rechercher%20sur%20judilibre"
+        self.date_du = ""
+        self.date_au = ""
+        self.search_api_fulltext = ""
+        self.sort = "date-desc"
+        self.items_per_page = 30
+        self.session = HTMLSession()
 
-	def search(self, pages=1):
-		html = list()
-		for page in range(pages):
-			url = self.base_search_url.format(
-				url=self.base_url,
-				search_api_fulltext=self.search_api_fulltext,
-				date_du=self.date_du,
-				date_au=self.date_au,
-				juridiction=self.juridiction,
-				op=self.op,
-				sort=self.sort,
-				items_per_page=self.items_per_page,
-				page=page
-			)
-			html.append(self.get_html(url))
-		return '\n'.join(html)
+    def search(self, pages=1):
+        html = list()
+        for page in range(pages):
+            url = self.base_search_url.format(
+                url=self.base_url,
+                search_api_fulltext=self.search_api_fulltext,
+                date_du=self.date_du,
+                date_au=self.date_au,
+                juridiction=self.juridiction,
+                op=self.op,
+                sort=self.sort,
+                items_per_page=self.items_per_page,
+                page=page,
+            )
+            html.append(self.get_html(url))
+        return "\n".join(html)
 
-	def decision(self, id):
-		url = self.base_decision_url.format(
-			url=self.base_url,
-			id=id
-		)
-		return self.get_html(url)
+    def decision(self, id):
+        url = self.base_decision_url.format(url=self.base_url, id=id)
+        return self.get_html(url)
 
-	def decisions(self, pages=1):
-		html = self.search(pages)
-		decisions = list()
-		for m in regexps.decision_ref_re.finditer(html):
-			decisions.append(m.group('id'))
-		for f in decisions:
-			print(f'"{f}",')
-		return decisions
+    def decisions(self, pages=1):
+        html = self.search(pages)
+        decisions = list()
+        for m in regexps.decision_ref_re.finditer(html):
+            decisions.append(m.group("id"))
+        for f in decisions:
+            print(f'"{f}",')
+        return decisions
 
-	def get_html(self, url):
-		response = self.session.get(url)
-		response.html.render()
-		return response.html.html
+    def get_html(self, url):
+        response = self.session.get(url)
+        response.html.render()
+        return response.html.html
--- tp_regexps/scripts/get_judilibre.py
+++ tp_regexps/scripts/get_judilibre.py
@@ -8,115 +8,122 @@ from tp_regexps.judilibre import Judilibre
 
 
 def download(filename, data):
-	with open(filename, 'w') as f:
-		f.write(data)
-	print(f"wrote to {filename}")
+    with open(filename, "w") as f:
+        f.write(data)
+    print(f"wrote to {filename}")
 
 
 def main():
-	parser = argparse.ArgumentParser()
-	parser.add_argument("operation", choices=(
-		'search', 'decision', 'decisions', 'read_decision',
-		'write_test_decisions', 'parse_html_decision',
-		'parse_html_decisions'
-	))
-	parser.add_argument("param1", default=None, nargs='?')
-	parser.add_argument("-d", "--download", default=False, action='store_true')
-	parser.add_argument("-f", "--force", default=False, action='store_true')
-	args = parser.parse_args()
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "operation",
+        choices=(
+            "search",
+            "decision",
+            "decisions",
+            "read_decision",
+            "write_test_decisions",
+            "parse_html_decision",
+            "parse_html_decisions",
+        ),
+    )
+    parser.add_argument("param1", default=None, nargs="?")
+    parser.add_argument("-d", "--download", default=False, action="store_true")
+    parser.add_argument("-f", "--force", default=False, action="store_true")
+    args = parser.parse_args()
 
-	judilibre = Judilibre()
+    judilibre = Judilibre()
 
-	if args.operation == 'search':
-		count = int(args.param1) if args.param1 else 0
-		if count > 0:
-			pages = count // judilibre.items_per_page
-			remaining = count % judilibre.items_per_page
-			if remaining > 0:
-				pages += 1
-		else:
-			pages = 1
-		data = judilibre.search(pages)
-		if args.download:
-			download("search.html", data)
-		else:
-			print(data)
-	elif args.operation == 'decision':
-		id = args.param1
-		if id is None:
-			print("ERROR: missing decision id", file=sys.stderr)
-			return 1
-		data = judilibre.decision(id)
-		if args.download:
-			download(f"{id}.html", data)
-		else:
-			print(data)
-	elif args.operation == 'write_test_decisions':
-		path = args.param1
-		if path is None:
-			print("ERROR: missing test decisions path", file=sys.stderr)
-			return 1
-		for filename in os.listdir(path):
-			if filename.endswith(".html"):
-				id = filename[:-5]
-				if id == '6757dc458b75c64649d25972':
-					print(f"skip {id}")
-					continue
-				html_filename = os.path.join(path, filename)
-				with open(html_filename, 'r') as f:
-					html_data = f.read()
-				html_decision = Decision.from_html(id, html_data)
-				json_filename = Path(path) / f"{id}.json"
-				if not os.path.exists(json_filename) or args.force:
-					with open(json_filename, 'w') as f:
-						f.write(html_decision.to_json())
-						print(f"wrote to {json_filename}")
-	elif args.operation == 'read_decision':
-		path = args.param1
-		if path is None:
-			print("ERROR: missing decision path", file=sys.stderr)
-			return 1
-		with(open(path, 'r')) as f:
-			data = f.read()
-		decision = Decision.from_json(data)
-		print(decision.to_json())
-	elif args.operation == 'parse_html_decision':
-		path = args.param1
-		if path is None:
-			print("ERROR: missing decision path", file=sys.stderr)
-			return 1
-		with open(path, 'r') as f:
-			html_data = f.read()
-		id = os.path.basename(path)[:-5]
-		html_decision = Decision.from_html(id, html_data)
-		print(html_decision.to_json())
-	elif args.operation == 'parse_html_decisions':
-		path = args.param1
-		if path is None:
-			print("ERROR: missing decisions path", file=sys.stderr)
-			return 1
-		for filename in os.listdir(path):
-			if filename.endswith(".html"):
-				html_path = os.path.join(path, filename)
-				with open(html_path, 'r') as f:
-					html_data = f.read()
-				id = filename[:-5]
-				html_decision = Decision.from_html(id, html_data)
-				print(html_decision.to_json())
-	elif args.operation == 'decisions':
-		count = int(args.param1) if args.param1 else 0
-		if count > 0:
-			pages = count // judilibre.items_per_page
-			remaining = count % judilibre.items_per_page
-			if remaining > 0:
-				pages += 1
-		else:
-			pages = 1
-		decisions = judilibre.decisions(pages)
-		if args.download:
-			for id in decisions:
-				decision = judilibre.decision(id)
-				download(f"{id}.html", decision)
-		else:
-			print(decisions)
-	return 0
+    if args.operation == "search":
+        count = int(args.param1) if args.param1 else 0
+        if count > 0:
+            pages = count // judilibre.items_per_page
+            remaining = count % judilibre.items_per_page
+            if remaining > 0:
+                pages += 1
+        else:
+            pages = 1
+        data = judilibre.search(pages)
+        if args.download:
+            download("search.html", data)
+        else:
+            print(data)
+    elif args.operation == "decision":
+        id = args.param1
+        if id is None:
+            print("ERROR: missing decision id", file=sys.stderr)
+            return 1
+        data = judilibre.decision(id)
+        if args.download:
+            download(f"{id}.html", data)
+        else:
+            print(data)
+    elif args.operation == "write_test_decisions":
+        path = args.param1
+        if path is None:
+            print("ERROR: missing test decisions path", file=sys.stderr)
+            return 1
+        for filename in os.listdir(path):
+            if filename.endswith(".html"):
+                id = filename[:-5]
+                if id == "6757dc458b75c64649d25972":
+                    print(f"skip {id}")
+                    continue
+                html_filename = os.path.join(path, filename)
+                with open(html_filename, "r") as f:
+                    html_data = f.read()
+                html_decision = Decision.from_html(id, html_data)
+                json_filename = Path(path) / f"{id}.json"
+                if not os.path.exists(json_filename) or args.force:
+                    with open(json_filename, "w") as f:
+                        f.write(html_decision.to_json())
+                        print(f"wrote to {json_filename}")
+    elif args.operation == "read_decision":
+        path = args.param1
+        if path is None:
+            print("ERROR: missing decision path", file=sys.stderr)
+            return 1
+        with open(path, "r") as f:
+            data = f.read()
+        decision = Decision.from_json(data)
+        print(decision.to_json())
+    elif args.operation == "parse_html_decision":
+        path = args.param1
+        if path is None:
+            print("ERROR: missing decision path", file=sys.stderr)
+            return 1
+        with open(path, "r") as f:
+            html_data = f.read()
+        id = os.path.basename(path)[:-5]
+        html_decision = Decision.from_html(id, html_data)
+        print(html_decision.to_json())
+    elif args.operation == "parse_html_decisions":
+        path = args.param1
+        if path is None:
+            print("ERROR: missing decisions path", file=sys.stderr)
+            return 1
+        for filename in os.listdir(path):
+            if filename.endswith(".html"):
+                html_path = os.path.join(path, filename)
+                with open(html_path, "r") as f:
+                    html_data = f.read()
+                id = filename[:-5]
+                html_decision = Decision.from_html(id, html_data)
+                print(html_decision.to_json())
+    elif args.operation == "decisions":
+        count = int(args.param1) if args.param1 else 0
+        if count > 0:
+            pages = count // judilibre.items_per_page
+            remaining = count % judilibre.items_per_page
+            if remaining > 0:
+                pages += 1
+        else:
+            pages = 1
+        decisions = judilibre.decisions(pages)
+        if args.download:
+            for id in decisions:
+                decision = judilibre.decision(id)
+                download(f"{id}.html", decision)
+        else:
+            print(decisions)
+    return 0
