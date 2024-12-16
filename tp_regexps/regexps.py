e918ef14b295dd57ed723a60249f8a50992d39be
LilianBernot <lilian.bernot@gmail.com>
2024-12-16 14:43:53 +0100

    feat(regexps): initiate regex todo for all metadata

--- tp_regexps/regexps.py
+++ tp_regexps/regexps.py
@@ -2,3 +2,11 @@ import re
 
 decision_ref_re = re.compile(r'<a\s+href="/decision/(?P<id>[^\?]+)')
 ecli_re = re.compile(r"(?P<ecli>ECLI:[A-Z]+:[A-Z]+:\d{4}:[A-Z0-9]+)")
+chamber_re = re.compile(r"TODO")
+formation_re = re.compile(r"TODO")
+publication_re = re.compile(r"TODO")
+number_re = re.compile(r"TODO")
+decision_date_re = re.compile(r"TODO")
+solution_re = re.compile(r"TODO")
+content_re = re.compile(r"TODO")
+texts_re = re.compile(r"TODO")
