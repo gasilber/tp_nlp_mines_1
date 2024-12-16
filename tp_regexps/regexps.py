0a821264821f32d12bb1199d0b93552debcc4b3f
LilianBernot <lilian.bernot@gmail.com>
2024-12-16 15:34:17 +0100

    feat(regexps): write formation regex

--- tp_regexps/regexps.py
+++ tp_regexps/regexps.py
@@ -3,7 +3,7 @@ import re
 decision_ref_re = re.compile(r'<a\s+href="/decision/(?P<id>[^\?]+)')
 ecli_re = re.compile(r"(?P<ecli>ECLI:[A-Z]+:[A-Z]+:\d{4}:[A-Z0-9]+)")
 chamber_re = re.compile(r'class="h4-like">(?P<chamber>.*)')
-formation_re = re.compile(r"TODO")
+formation_re = re.compile(r'class="h4-like">.*\s*\-\s*(?P<formation>.*)')
 publication_re = re.compile(r"TODO")
 number_re = re.compile(r"TODO")
 decision_date_re = re.compile(r"TODO")
