60ca6d06b8023d154c21c7169d79600f0c37848e
LilianBernot <lilian.bernot@gmail.com>
2024-12-16 15:11:18 +0100

    feat(regexps): write chamber regex

--- tp_regexps/regexps.py
+++ tp_regexps/regexps.py
@@ -2,7 +2,7 @@ import re
 
 decision_ref_re = re.compile(r'<a\s+href="/decision/(?P<id>[^\?]+)')
 ecli_re = re.compile(r"(?P<ecli>ECLI:[A-Z]+:[A-Z]+:\d{4}:[A-Z0-9]+)")
-chamber_re = re.compile(r"TODO")
+chamber_re = re.compile(r'class="h4-like">(?P<chamber>.*)')
 formation_re = re.compile(r"TODO")
 publication_re = re.compile(r"TODO")
 number_re = re.compile(r"TODO")
